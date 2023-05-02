59a0b4cfc3f43e890ef3d4e3c99b7a9efb4d448f
patriciojavier16 <patriciomoralespaucar@gmail.com>
2023-05-02 07:35:35 -0500

    Pagina realizada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { ServiciosComponent } from './componentes/servicios/servicios.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { ContactosComponent } from './componentes/contactos/contactos.component';
+import { E404Component } from './componentes/e404/e404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'contactos', component: ContactosComponent },
+  { path: '**', component: E404Component },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { ServiciosComponent } from './componentes/servicios/servicios.component';
+import { NosotrosComponent } from './componentes/nosotros/nosotros.component';
+import { ContactosComponent } from './componentes/contactos/contactos.component';
+import { E404Component } from './componentes/e404/e404.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ServiciosComponent,
+    NosotrosComponent,
+    ContactosComponent,
+    E404Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
